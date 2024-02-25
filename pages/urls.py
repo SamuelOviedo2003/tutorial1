50996934bd5081fed80583d7a5250b3c7c5663c7
SamuelOviedo2003 <92952464+SamuelOviedo2003@users.noreply.github.com>
2024-02-25 18:47:08 -0500

    tercer tutorial

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path 
-from .views import HomePageView,ContactPageView,AboutPageView,ProductIndexView,ProductShowView,ProductCreateView
+from django.urls import path
+
+from pages.utils import ImageLocalStorage 
+from .views import CartRemoveAllView, CartView, HomePageView,ContactPageView,AboutPageView, ImageViewFactory, ImageViewNoDI,ProductIndexView,ProductShowView,ProductCreateView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name="home"),
@@ -8,5 +10,12 @@ urlpatterns = [
     path('products/create', ProductCreateView.as_view(), name='form'), 
     path('products/<str:id>', ProductShowView.as_view(), name='show'),
     path('contact/', ContactPageView.as_view(), name='contact'),
+    path('cart/', CartView.as_view(), name='cart_index'), 
+    path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'), 
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'), 
+    path('image/', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_index'), 
+    path('image/save', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_save'),
+    path('imagenotdi/', ImageViewNoDI.as_view(), name='imagenodi_index'), 
+    path('image/save', ImageViewNoDI.as_view(), name='imagenodi_save'),  
     
     ] 
